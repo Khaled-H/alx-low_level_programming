b41b5ad44ed1368ca94bc4d29bf8a0da52b24bbc
Khaled Elhusseiny <khaled.hussieny@gmail.com>
2023-12-15 14:35:41 -0800

    Adding 6-size.c file

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - This programm intended to pring the size of datatypes
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+char a;
+int b;
+long int c;
+long long d;
+float e;
+printf("Size of a char: %lu Byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of a int: %lu Byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long int: %lu Byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long long: %lu Byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a float: %lu Byte(s)\n", (unsigned long)sizeof(e));
+return (0);
+}
