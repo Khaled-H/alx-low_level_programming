432dedff1c7048ae99c44d29879003e267f3fcd5
Khaled Elhusseiny <khaled.hussieny@gmail.com>
2023-12-15 13:52:00 -0800

    modifying the file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 /**
- * main - Aprogram to print a message
- *
- * Return: Always 0 (Success)
+ * main - A program to print a message
+ *Return: Always 0 (Success)
  */
-
-int main (void)
+int main(void)
 {	
-	puts("\"Programming is like building a multilingual puzzle");
-	
+puts("\"Programming is like building a multilingual puzzle");	
 return (0);
 }
-
 
