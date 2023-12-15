4741a8862baa496ac31bc24446033680b920105c
Khaled Elhusseiny <khaled.hussieny@gmail.com>
2023-12-15 13:42:00 -0800

    Adding 4-puts.c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Aprogram to print a message
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main (void)
+{	
+	puts("\"Programming is like building a multilingual puzzle");
+	
+return (0);
+}
+
+
