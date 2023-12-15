cde52223873e0a4a1b76f46fbccbee11cdbb5776
Khaled Elhusseiny <khaled.hussieny@gmail.com>
2023-12-15 14:08:34 -0800

    adding 5-printf.c file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - a program to print a line of text
+ *
+ * return 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
