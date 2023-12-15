8494a69d383f38aa56b476de0d793318d47b6d31
Khaled Elhusseiny <khaled.hussieny@gmail.com>
2023-12-15 14:51:54 -0800

    post compile

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,7 +9,7 @@ int main(void)
 char a;
 int b;
 long int c;
-long long d;
+long long int d;
 float e;
 printf("Size of a char: %lu Byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of an int: %lu Byte(s)\n", (unsigned long)sizeof(b));
